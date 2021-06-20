0cac32e29498071638b4723d2693c9bd229223e2
EKI-INDRADI <eki.indradi@gmail.com>
2021-06-20 15:32:35 +0700

    enable global request limit  1 request/second

--- server.js
+++ server.js
@@ -73,7 +73,6 @@ db.mongoose
 // =============================== CONNECTION BY VAR
 
 
-
 const enable_rate_limit_1sec_per_second = rateLimit({
   // windowMs: 60 * 60 * 1000, // 1 hour window
   // max: 5, // start blocking after 5 requests
@@ -83,14 +82,27 @@ const enable_rate_limit_1sec_per_second = rateLimit({
   windowMs: 1000, // 1 second
   max:1, // start blocking after 1 requests
   message:
-    {status : 0, rate_limit : "block", message :"Too many request from this IP, please try again after an second" }
+    {status : 0, request_limit : "block", message :"Too many request from this IP, please try again after an second" }
 });
+
+
+
+
 
 
 // 16-june-2021 ============ enable rate limit
-app.get("/rate-limit",enable_rate_limit_1sec_per_second,  (req, res) => {
-  res.json({ message: "Rate limit  1 sec OK" });
-});
+
+
+
+// ---------- global rate limit
+app.use(enable_rate_limit_1sec_per_second);
+// ---------- / global rate limit
+
+// ----------  spesific rate limit
+// app.get("/rate-limit",enable_rate_limit_1sec_per_second,  (req, res) => {
+//   res.json({ message: "Rate limit  1 sec OK" });
+// });
+// ---------- / spesific rate limit
 // 16-june-2021 ============ enable rate limit
 
 
